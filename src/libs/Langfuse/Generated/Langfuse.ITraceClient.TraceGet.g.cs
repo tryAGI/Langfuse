1cac909502c3e8b572b6f6dd1ba3d2d1de1b5a61
Konstantin S. <havendv@gmail.com>
2026-04-08 20:10:26 +0400

    feat: Updated OpenAPI spec (#25)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Langfuse/Generated/Langfuse.ITraceClient.TraceGet.g.cs
+++ src/libs/Langfuse/Generated/Langfuse.ITraceClient.TraceGet.g.cs
@@ -8,10 +8,12 @@ namespace Langfuse
         /// Get a specific trace
         /// </summary>
         /// <param name="traceId"></param>
+        /// <param name="fields"></param>
         /// <param name="cancellationToken">The token to cancel the operation with</param>
         /// <exception cref="global::Langfuse.ApiException"></exception>
         global::System.Threading.Tasks.Task<global::Langfuse.TraceWithFullDetails> TraceGetAsync(
             string traceId,
+            string? fields = default,
             global::System.Threading.CancellationToken cancellationToken = default);
     }
 }
